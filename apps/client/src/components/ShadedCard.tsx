fcc4cc6bd0579d8b12982655c1d148edb39c3296
Nicholas Gregory <nicholas.babcock092@gmail.com>
2026-02-07 16:45:22 -0800

    Improvement: Component: ShadedCard; (tailwind refactor)

--- apps/client/src/components/ShadedCard.tsx
+++ apps/client/src/components/ShadedCard.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import { shadedBorders } from "../utils/color";
 
 export const ShadedCard: React.FC<{ color: string, children: React.ReactNode }> = ({ color, children }) => {
+  const colors: any = {
+    cyan: 'bg-cyan-500',
+    green: 'bg-green-500'
+  }
+
+  const borderColors: any = {
+    cyan: ['border-t-cyan-400', 'border-l-cyan-400', 'border-b-cyan-600', 'border-r-cyan-600'],
+    green: ['border-t-green-400', 'border-l-green-400', 'border-b-green-600', 'border-r-green-600']
+  }
+
   return (
     <div
-      className="border-5 border-radius-2 card-shadow p-1"
-      style={{
-        backgroundColor: color,
-        ...shadedBorders(color, 50)
-      }}
+      className={`border-5 rounded-xl ${colors[color]} p-3 ${borderColors[color].join(" ")}`}
     >
       {children}
     </div>
